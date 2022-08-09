67c170b81c2aa6b0e3dc0fc7102a5d41a751ab83
JulianKleist <julianlopez.j.l.52@gmail.com>
2022-08-09 11:12:22 -0300

    Titles

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HomeScreen from './src/Screens/HomeScreen';
+import AppLoading from 'expo-app-loading';
+import {
+    useFonts,
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_700Bold,
+} from '@expo-google-fonts/playfair-display';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
-  );
+  let [fontsLoaded] = useFonts({
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    );
+  }
 }
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    
+    return (
+        <View>
+            <Text style={styles.greetings}>Hey, Julian!</Text>
+            <Text style={styles.title}>Where would you like to travel today?</Text>
+            <Text>Buttons</Text>
+            <Text>Destinations</Text>
+        </View>
+    )
 }
+
+
+const styles = StyleSheet.create({
+    greetings: {
+        color: '#A8A8A8',
+        fontSize: 16
+    },
+    title: {
+        color: '#26355A',
+        marginVertical: 15,
+        fontSize: 29,
+        fontFamily: 'PlayfairDisplay_700Bold'
+    },
+})
